b08a1f9e8182c9a7759655141994d29db7806ee9
rahul <rahul.yadav@fareclubs.com>
2026-02-11 19:07:14 +0530

    error fix

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -3,15 +3,7 @@ from .base import *  # noqa: F403
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
-INSTALLED_APPS += [
-    "debug_toolbar",
-    "django_extensions",
-    "django_stubs_ext",
-]
 
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
 
 
 # TEMPORARY: Use R2 in development for testing
